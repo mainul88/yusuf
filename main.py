b92f6cd89106c64f4a349b9cbae10328366261d9
Mainul <mainulkadr@gmail.com>
2023-03-31 00:03:08 -0400

    first comment

---
+++ main.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
+
+app = Flask(__name__)
+
+conn_str = "mysql://root:5676@localhost/boatdb"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
+
+
+@app.route('/boats')
+def get_boats():
+    # local_session = Session(bind=engine)
+    # boats = local_session.query(BoatsModel).all()  # returns all boats
+    boats = conn.execute(text("select * from boats")).all()
+    print(boats)
+    return render_template('boats.html', boats=boats[:10])
+
+
+@app.route('/create', methods=['GET'])
+def create_get_request():
+    return render_template('boats_create.html')
+
+
+@app.route('/create', methods=['POST'])
+def create_boat():
+    try:
+        conn.execute(
+            text("INSERT INTO boats values (:id, :name, :type, :owner_id, :rental_price)"),
+            request.form
+        )
+        return render_template('boats_create.html', error=None, success="Data inserted successfully!")
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('boats_create.html', error=error, success=None)
+
+
+@app.route('/delete', methods=['GET'])
+def create_delete_request():
+    return render_template('boats_delete.html')
+
+
+@app.route('/delete', methods=['POST'])
+def delete_boat():
+    # you can access the values with request.from.name
+    # this name is the value of the name attribute in HTML form's input element
+    # ex: print(request.form['id'])
+    # boats_delete.html
+    try:
+        conn.execute(
+            text("DELETE FROM boats WHERE id = :id"),
+            request.form
+        )
+        return render_template('boats_delete.html', error=None, success="Data inserted successfully!")
+    except Exception as e:
+        error = e.orig.args[1]
+        print(error)
+        return render_template('boats_delete.html', error=error, success=None)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
